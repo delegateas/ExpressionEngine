06f3ee12bc91b9d442038a96d9db46c9db821f48
Thyge S. Steffensen <thygesteffensen@users.noreply.github.com>
2022-03-04 13:10:51 +0100

    Applied changes from comment

--- ExpressionEngine/IFunctionDefinition.cs
+++ ExpressionEngine/IFunctionDefinition.cs
@@ -1,8 +1,18 @@
 namespace ExpressionEngine
 {
+    /// <summary>
+    /// Interface to function definitions
+    /// </summary>
     public interface IFunctionDefinition
     {
-        public string From();
-        public string To();
+        /// <summary>
+        /// The name of the 'function' which is replaced
+        /// </summary>
+        string From { get; set; }
+        
+        /// <summary>
+        /// What it is replaced with 
+        /// </summary>
+        string To { get; set; }
     }
 }
